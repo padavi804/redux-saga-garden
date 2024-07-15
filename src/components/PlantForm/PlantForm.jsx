a8c7c924bb5a5294370e737fb76b66545dda1c6f
Patrick Davison <davisonpatrickj@gmail.com>
2024-07-14 16:01:06 -0800

    update notes

--- src/components/PlantForm/PlantForm.jsx
+++ src/components/PlantForm/PlantForm.jsx
@@ -1,30 +1,70 @@
-import React, { useState }from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 const PlantForm = () => {
     const dispatch = useDispatch();
-    
-    //Initial state is an OBJECT, with keys id and name
-    let [newPlant, setPlant] = useState({id: 4, name: ''});
 
-    const handleNameChange = (event) => {
-        //Similar to in redux -- we dont want to get rid of the id field when we update name
-        setPlant({...newPlant, name: event.target.value})
-    }
+    let [name, setName] = useState('');
+    let [kingdom, setKingdom] = useState('');
+    let [clade, setClade] = useState('');
+    let [order, setOrder] = useState('');
+    let [family, setFamily] = useState('');
+    let [subfamily, setSubfamily] = useState('');
+    let [genus, setGenus] = useState('');
 
     const addNewPlant = event => {
         event.preventDefault();
-        dispatch({ type: 'ADD_PLANT', payload: newPlant });
-        //updates the next plant to have a new id
-        setPlant({id:newPlant.id + 1, name: ''});
+        dispatch({ type: 'SEND_PLANTS', payload: { name, kingdom, clade, order, family, subfamily, genus } });
+        //Clear out input fields
+        setName('');
+        setKingdom('');
+        setClade('');
+        setOrder('');
+        setFamily('');
+        setSubfamily('');
+        setGenus('');
     }
     return (
         <div>
             <h3>This is the form</h3>
-            <pre>{JSON.stringify(newPlant)}</pre>
             <form onSubmit={addNewPlant}>
-                <input type='text' value={newPlant.name} onChange={handleNameChange} />
+                <label>
+                    Name
+                    <input type='text' value={name} onChange={(event) => setName(event.target.value)} required />
+                </label>
+
+                <label>
+                    Kingdom
+                    <input type='text' value={kingdom} onChange={(event) => setKingdom(event.target.value)} />
+                </label>
+
+                <label>
+                    Clade
+                    <input type='text' value={clade} onChange={(event) => setClade(event.target.value)} />
+                </label>
+
+                <label>
+                    Order
+                    <input type='text' value={order} onChange={(event) => setOrder(event.target.value)} />
+                </label>
+
+                <label>
+                    Family
+                    <input type='text' value={family} onChange={(event) => setFamily(event.target.value)} />
+                </label>
+
+                <label>
+                    Subfamily
+                    <input type='text' value={subfamily} onChange={(event) => setSubfamily(event.target.value)} />
+                </label>
+
+                <label>
+                    Genus
+                    <input type='text' value={genus} onChange={(event) => setGenus(event.target.value)} />
+                </label>
+
                 <input type='submit' value='Add New Plant' />
+
             </form>
         </div>
     );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,67 +1,73 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 import { takeEvery, put } from 'redux-saga/effects';
+
 import axios from 'axios';
 
+function* fetchPlants(action) {
+  console.log("fetching plants, due to action:", action);
 
-// this startingPlantArray should eventually be removed
-const startingPlantArray = [
-  { id: 1, name: 'Rose' },
-  { id: 2, name: 'Tulip' },
-  { id: 3, name: 'Oak' }
-];
+  try {
+    const serverResponse = yield axios.get('/api/plants');
+    console.log('serverResponse:', serverResponse);
+    yield put({ type: 'SET_PLANTLIST', payload: serverResponse.data });
+  } catch (error) {
+    console.log("Error fetching plants from the server");
+  }
+}
+
+function* sendPlants(action) {
+  console.log("posting plants, due to action:", action);
+
+  try {
+    const serverResponse = yield axios({ method: 'POST', url: '/api/plants', data: action.payload });
+    console.log('serverResponse:', serverResponse);
+    yield put({ type: 'FETCH_PLANTS' });
+  } catch (error) {
+    console.log("Error posting plants to the server");
+  }
+}
+
+function* deletePlants(action) {
+  console.log("deleting plants, due to action:", action);
+
+  try {
+    const serverResponse = yield axios({ method: 'DELETE', url: `/api/plants/${action.payload}` });
+    console.log('serverResponse:', serverResponse);
+    yield put({ type: 'FETCH_PLANTS' });
+  } catch (error) {
+    console.log("Error deleting plant from the server");
+  }
+}
+
+function* rootSaga() {
+  yield takeEvery('FETCH_PLANTS', fetchPlants);
+  yield takeEvery('SEND_PLANTS', sendPlants);
+  yield takeEvery('REMOVE_PLANTS', deletePlants);
+}
 
 const plantList = (state = [], action) => {
   switch (action.type) {
-    case 'ADD_PLANT':
-      return [...state, action.payload]
-    case 'FETCH_PLANT':
-      return action.payload;
+    case 'SET_PLANTLIST':
+      return action.payload
     default:
       return state;
   }
 };
 
-function* fetchPlants() {
-  try {
-    const plantResponse = yield axios.get('/api/plants');
-    yield put({ type: 'ADD_PLANT', payload: plantResponse.data });
-  }
-  catch (error) {
-    console.log('Error fetching plants', error);
-  }
-}
+const sagaMiddleware = createSagaMiddleware();
 
-function* sendPlants(action) {
-  try {
-    console.log('action.payload', action.payload);
-    const plantResponse = yield axios({ method: 'POST', url: '/api/plants', data: { name: action.payload } });
-    yield put({ type: 'FETCH_PLANTS' })
-  }
-  catch (error) {
-    console.log('Error fetching elements', error);
-  }
-}
-
-
-function* rootSaga() {
-  yield takeEvery('ADD_PLANT', fetchPlants)
-  yield takeEvery('FETCH_PLANTS', sendPlants)
-}
 // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
 // Note that the store is currently not
 // configured to utilize redux-saga OR
 // redux logger!
-const sagaMiddleware = createSagaMiddleware();
-
-
 const store = createStore(
   combineReducers({ plantList }),
-  applyMiddleware(sagaMiddleware, logger),
+  applyMiddleware(logger, sagaMiddleware)
 );
 // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
-sagaMiddleware.run(rootSaga);
 
+sagaMiddleware.run(rootSaga);
 
 export default store;
